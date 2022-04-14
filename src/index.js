8e2d2446650e2847266d99e8e8a43d54500f0b55
yu-M215 <yuu0609km.215@gmail.com>
2022-04-14 19:59:14 +0900

    [Update] コンストラクタを追加して、stateを初期化する

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => console.log('click'); }>
+      <button className="square" onClick={() => console.log('click') }>
         {this.props.value}
       </button>
     );
