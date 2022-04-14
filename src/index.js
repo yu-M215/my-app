5bc00b3839225c686f2cae2d2f161124fa02da61
yu-M215 <yuu0609km.215@gmail.com>
2022-04-14 20:46:41 +0900

    [Update] ()を{}に修正

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">(status)</div>
+        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
